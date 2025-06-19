7570c42a6e5eae6fd943cfb4017b8fb7056c72e5
김창현/배포플랫폼개발팀/CL <changhyun.kim@nhn.com>
2025-06-19 11:46:12 +0900

    오류시 Content-Type을 problem+json 형태로 수정

--- src/main/java/com/nhncloud/pca/exception/GlobalExceptionHandler.java
+++ src/main/java/com/nhncloud/pca/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.nhncloud.pca.exception;
 
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,7 +17,10 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(MethodNotAllowedException.class)
     public HttpEntity<Problem> handleMethodNotAllowedException(MethodNotAllowedException ex) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_PROBLEM_JSON);
         return ResponseEntity.status(ex.getStatusCode())
+                .headers(headers)
                 .body(new Problem(ProblemType.MALFORMED));
     }
 }
